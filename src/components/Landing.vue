207f72d0fa0b1bffaaee0cce539cea3cf9b27641
Silvia O'Dwyer <silviaodwyer119@yahoo.ie>
2019-01-07 15:24:20 +0000

    Landing component addition.

---
+++ src/components/Landing.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <div>
+      <h1>xq</h1>
+      <p>A live markdown editor and storage bin based on <a href="https://blockstack.org" target="_blank">Blockstack</a></p>
+      <button class="btn btn-default" @click.prevent="signIn">Sign In With Blockstack</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'landing',
+  data () {
+    return {
+      blockstack: window.blockstack
+    }
+  },
+
+  methods: {
+    signIn () {
+      const blockstack = this.blockstack
+      blockstack.redirectToSignIn()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
