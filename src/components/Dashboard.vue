5bdd2cf7160987e01b9c1f1c7fbbe1b145cc5d2b
Silvia O'Dwyer <silviaodwyer119@yahoo.ie>
2019-01-07 14:38:43 +0000

    Re-used saveCode function from my code editor codevault repo.

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-   <el-container height="500px">
+   <el-container class="dashboard">
       <el-header style="text-align: right; font-size: 12px">
          <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
             <el-menu-item index="1">File</el-menu-item>
@@ -16,10 +16,14 @@
                </el-submenu>
             </el-submenu>
             <el-menu-item index="3" disabled>Info</el-menu-item>
+            <span>
+              <img :src="user.avatarUrl() ? user.avatarUrl() : '/avatar-placeholder.png'" class="avatar" height="30px" width="30px">
+              <small>
+                <span class="sign-out">(<a href="#" @click.prevent="signOut">Sign Out</a>)</span>
+              </small>
+         </span>
          </el-menu>
-         <img :src="user.avatarUrl() ? user.avatarUrl() : '/avatar-placeholder.png'" class="avatar" height="30px" width="30px">
-         <span>            
-         <small><span class="sign-out">(<a href="#" @click.prevent="signOut">Sign Out</a>)</span></small></span>
+         
       </el-header>
       <el-container>
          <el-aside width="200px">
@@ -29,7 +33,7 @@
                      <template slot="title"><i class="el-icon-message"></i>Your Notes</template>
                      <el-menu-item-group>
                         <template slot="title">Recently Added</template>
-                        <el-menu-item index="1-1">Note1.md</el-menu-item>
+                        <el-menu-item index="1-1" v-for="note in markdown_notes">{{note}}</el-menu-item>
                      </el-menu-item-group>
                      <el-menu-item-group title="Group 2">
                         <el-menu-item index="1-3">Option 3</el-menu-item>
@@ -43,16 +47,10 @@
             </el-aside>
          </el-aside>
          <el-main>
+           <el-input placeholder="Note Name" v-model="filename"></el-input>
             <codemirror v-model="content" id="editor" :options="cmOptions"></codemirror>
-            <form @submit.prevent="saveNote()" :disabled="! content">
-               <div class="input-group">
-                  <input v-model="todo" type="text" class="form-control" placeholder="Paste your code..." autofocus>
-                  <span class="input-group-btn">
-                  <button class="btn btn-default" type="submit" :disabled="! todo">Add</button>
-                  </span>
-               </div>
-            </form>
-            <ul class="list-group">
+
+            <!-- <ul class="list-group">
                <li v-for="note in md_notes"
                   class="list-group-item"
                   :class="{completed: note.completed}"
@@ -64,7 +62,18 @@
                      class="delete pull-right"
                      href="#">X</a>
                </li>
-            </ul>
+            </ul> -->
+            <section class="preview" v-model="markdownToHTML">
+            {{markdownToHTML}}
+            </section>
+                <form @submit.prevent="saveNote()" :disabled="! content">
+               <div class="input-group">
+                  <span class="input-group-btn">
+                  <button class="btn btn-default" type="submit" :disabled="! content">Save</button>
+                  </span>
+               </div>
+            </form>
+
          </el-main>
       </el-container>
    </el-container>
@@ -75,14 +84,7 @@ import {codemirror} from 'vue-codemirror'
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/base16-dark.css'
 import 'codemirror/theme/abcdef.css'
-
-// Import popular language modes
-import 'codemirror/mode/javascript/javascript.js'
-import 'codemirror/mode/python/python.js'
-import 'codemirror/mode/css/css.js'
 import 'codemirror/mode/markdown/markdown.js'
-import 'codemirror/mode/rust/rust.js'
-
 import 'codemirror/addon/selection/active-line.js'
 import 'codemirror/addon/selection/mark-selection.js'
 /*eslint-disable*/
@@ -94,15 +96,15 @@ export default {
   data () {
     return {
       blockstack: window.blockstack,
-      md_notes: [],
+      markdown_notes: ["Note 1", "Note 2", "Note 3"],
       todo: '',
       uidCount: 0,
-      content: "Sample note",
+      content: "# Sample note",
       color: "#890912",
       cmOptions: {
         // Configuring codemirror options.
         tabSize: 4,
-        mode: "javascript",
+        mode: "markdown",
         theme: 'base16-dark',
         lineNumbers: true,
         autofocus: true,
@@ -110,15 +112,27 @@ export default {
         line: true,
         lineWrapping: true
       }, 
+      activeIndex: 1,
+      alertMessage: "",
+      filename: "",
+      noMarkdownAlert: false, 
+      noFilenameAlert: false,
+      isNewFile: false 
+    }
+  },
+  computed: {
+    markdownToHTML() {
+      console.log(this.content)
+      return("To HTML:" + this.content); 
     }
   },
   watch: {
-    md_notes: {
-      handler: function (md_notes) {
+    markdown_notes: {
+      handler: function (markdown_notes) {
         const blockstack = this.blockstack
 
         // encryption is now enabled by default
-        return blockstack.putFile(STORAGE_FILE, JSON.stringify(md_notes))
+        return blockstack.putFile(STORAGE_FILE, JSON.stringify(markdown_notes))
       },
       deep: true
     }
@@ -127,28 +141,93 @@ export default {
     this.fetchData()
   },
   methods: {
-    saveNote () {
+    saveNote() {
+      console.log(this.content);
       if (!this.content.trim()) {
-        return
+        console.log("No markdown entered in the editor.")
+        this.noMarkdownAlert = true;  
+        return;
       }
-      this.md_notes.unshift({
-        id: this.uidCount++,
-        text: this.todo.trim(),
-        completed: false
-      })
-      this.todo = ''
+      else if (this.filename == "") {
+        // TODO: Add modal which pops up to alert the user to enter a title.
+        console.log("No title entered");
+        this.alertMessage = "No title entered!"
+        this.noFilenameAlert = true;
+      }
+
+      // Both code and title entered
+      else {
+
+        let datestamp = this.getDateStamp();
+        // If all_file_ids contains the current ID, then the file exists already
+        // Update the file, rather than create a new one
+        if (this.isNewFile) {
+          // Create a new file.
+
+          this.markdown_notes.unshift({
+            id: this.uidCount++,
+            hash_id: String(this.getDateNow()),
+            content: this.content.trim(),
+            filename: this.filename,
+            language: this.cmOptions.mode,
+            completed: false,
+            date: datestamp
+          })
+
+          console.log(this.markdown_notes);
+          this.alertMessage = `New file ${this.filename} saved!`;
+          this.isNewFile = false;
+        }
+
+        else {
+          console.log("file already exists");
+          console.log("this.file", this.file);
+
+          let current_file = this.codefiles.filter(file => file.hash_id == this.file.hash_id)[0];
+          console.log(current_file)
+
+          // Save code
+          current_file.code = this.code;
+
+          // Save title
+          this.updateTitle(current_file);
+
+          // Update language
+          current_file.language = this.cmOptions.mode;
+
+          // Update datestamp
+          current_file.datestamp = datestamp
+
+          this.alertMessage = "Saved file!"
+        }
+        console.log("LANGMODE", this.cmOptions.mode)
+        this.showSnackbar = true;
+        console.log("ALL FILE IDS AFTR", this.all_file_ids);
+      }
+    },
+    updateTitle(file) {
+      if (this.filename == "") {
+            // TODO: Add a modal popup saying that the title must not be empty.
+          }
+          else {
+            file.filename = this.filename;
+      }
+    },
+    getDateNow() {
+      let time = Date.now();
+      return time;
     },
 
     fetchData () {
       const blockstack = this.blockstack
       blockstack.getFile(STORAGE_FILE) // decryption is enabled by default
       .then((todosText) => {
-        var md_notes = JSON.parse(todosText || '[]')
-        md_notes.forEach(function (note, index) {
+        var markdown_notes = JSON.parse(todosText || '[]')
+        markdown_notes.forEach(function (note, index) {
           note.id = index
         })
-        this.uidCount = md_notes.length
-        this.md_notes = md_notes
+        this.uidCount = markdown_notes.length
+        this.markdown_notes = markdown_notes
       })
     },
     handleOpen(key, keyPath) {
@@ -175,27 +254,35 @@ export default {
   }
 
   .el-header, .el-footer {
-    background-color: #B3C0D1;
     color: #333;
     font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", Arial, sans-serif;
 
   }
   
   .el-aside {
-    background-color: #D3DCE6;
     color: #333;
     font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", Arial, sans-serif;
 
   }
   
   .el-main {
-    background-color: #E9EEF3;
     color: #333;
     font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", Arial, sans-serif;
-
+    padding: 5vh;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
   }
 
+.dashboard {
+  height: 100vh;
+}
 
+#editor {
+  width: 50%;
+  margin: 0;
+  padding: 0;
+}
   
 label {
   margin-bottom: 0;
