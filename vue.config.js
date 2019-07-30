d14cf0cc23c45019996cd59ee906da2d96cbb841
Silvia O'Dwyer <silviaodwyer119@yahoo.ie>
2019-07-30 16:14:16 +0100

    Added vue.config.js; headers now automatically added to dist dir.

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding'
+    },
+  },
+  lintOnSave: false
+
+}
