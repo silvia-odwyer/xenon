a40eec2f5ab25c36358584f55f53820e79492a29
Silvia O'Dwyer <silviaodwyer119@yahoo.ie>
2019-07-30 16:19:09 +0100

    Added .gitignore and babel config.

---
+++ src/components/Editor.vue
@@ -0,0 +1,21 @@
+<template>
+    <div contenteditable="true" @input="updateHTML" class="textareaRoot"></div>
+</template>
+<script>
+/*eslint-disable*/
+
+export default {
+  // Custom textarea
+  name: "textEditor",
+  props: ["value"],
+  mounted: function() {
+    this.$el.innerHTML = this.value;
+  },
+  methods: {
+    updateHTML: function(e) {
+      this.$emit("input", e.target.innerHTML);
+      console.log("change", e.target.innerHTML);
+    }
+  },
+};
+</script>
